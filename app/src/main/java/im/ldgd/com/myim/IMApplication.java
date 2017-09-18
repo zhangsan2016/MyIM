d8a1f4ede6fc3c2cee6a0c7a11be79c4b02cddd9
liu <602694077@qq.com>
2017-09-18 15:15:07 +0800

    2017年9月18日 15:14:16

---
+++ app/src/main/java/im/ldgd/com/myim/IMApplication.java
@@ -0,0 +1,33 @@
+package im.ldgd.com.myim;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
+/**
+ * Created by ldgd on 2017/9/18.
+ */
+
+public class IMApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+       /* MultiDex.install(this);
+        EMOptions options = new EMOptions();
+        options.setAcceptInvitationAlways(false);
+        options.setAutoAcceptGroupInvitation(false);*/
+
+        EMOptions options = new EMOptions();
+         // 默认添加好友时，是不需要验证的，改成需要验证
+        options.setAcceptInvitationAlways(false);
+          //初始化
+        EMClient.getInstance().init(this, options);
+         //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true);
+
+
+    }
+}
---
+++ app/src/main/java/im/ldgd/com/myim/controller/activity/SplashActivity.java
@@ -0,0 +1,66 @@
+package im.ldgd.com.myim.controller.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hyphenate.chat.EMClient;
+
+import im.ldgd.com.myim.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            // 如果当前Activit已经退出，不处理handler信息
+            if (isFinishing()) {
+                return;
+            }
+
+            // 判断进入主页面还是登录页面
+            toMainOrLogin();
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // 发送2s种延迟消息
+        handler.sendMessageDelayed(Message.obtain(), 2000);
+
+    }
+
+    // 判断进入主页面还是登录页面
+    private void toMainOrLogin() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                // 判断当前帐号是否已经登录过
+                if(EMClient.getInstance().isLoggedInBefore()){  // 登录过
+
+                    // 获取登录用户信息
+
+                    // 跳转到主页面
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+
+                }else{  // 没有登录过
+                    // 跳转到登录
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        }.start();
+
+
+    }
+}
