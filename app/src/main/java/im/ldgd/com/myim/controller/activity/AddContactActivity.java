6620e443eef02eab81ec552c2d2ddd506564aebe
liu <602694077@qq.com>
2017-09-29 16:00:14 +0800

    2017年9月29日 16:00:03

---
+++ app/src/main/java/im/ldgd/com/myim/controller/activity/AddContactActivity.java
@@ -0,0 +1,144 @@
+package im.ldgd.com.myim.controller.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.exceptions.HyphenateException;
+
+import im.ldgd.com.myim.R;
+import im.ldgd.com.myim.model.Model;
+import im.ldgd.com.myim.model.UserInfo;
+import im.ldgd.com.myim.utils.LogUtil;
+
+/**
+ * Created by ldgd on 2017/9/29.
+ */
+public class AddContactActivity extends Activity implements View.OnClickListener {
+    private EditText etAddName;
+    private ImageView ivAddPhoto;
+    private TextView tvAddName;
+    private Button btAddAdd;
+    private UserInfo userInfo;
+    private TextView tvAddFind;
+    private RelativeLayout rlAdd;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_add_contact);
+
+        findViews();
+
+    }
+
+    private void findViews() {
+        tvAddFind = (TextView) this.findViewById(R.id.tv_add_find);
+        etAddName = (EditText) findViewById(R.id.et_add_name);
+        rlAdd = (RelativeLayout) findViewById(R.id.rl_add);
+        ivAddPhoto = (ImageView) findViewById(R.id.iv_add_photo);
+        tvAddName = (TextView) findViewById(R.id.tv_add_name);
+        btAddAdd = (Button) findViewById(R.id.bt_add_add);
+
+        btAddAdd.setOnClickListener(this);
+        tvAddFind.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == btAddAdd) {
+            LogUtil.e("btAddAddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            // 添加好友
+            addFriend();
+
+
+        } else if (v == tvAddFind) {
+            LogUtil.e("btvAddFinddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            // 查找用户
+            find();
+        }
+    }
+
+    /**
+     * 添加好友
+     */
+    private void addFriend() {
+        Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+
+                // 去环信服务器添加好友
+                try {
+                    EMClient.getInstance().contactManager().addContact(userInfo.getName(), "添加好友");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddContactActivity.this, "发送添加好友邀请成功", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(AddContactActivity.this, "发送添加好友邀请失败" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 查找按钮的处理
+     */
+    private void find() {
+        // 获取name
+        final String name = etAddName.getText().toString().trim();
+
+        // 校验输入的名称
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(AddContactActivity.this, "输入的用户名称不能为空！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Model.getInstance().getGlobalThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                // 去服务器判断当前用户是否存在
+                userInfo = new UserInfo(name);
+
+                // 更新UI
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        rlAdd.setVisibility(View.VISIBLE);
+                        tvAddName.setText(userInfo.getName());
+                    }
+                });
+
+
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/im/ldgd/com/myim/controller/fragment/ContactListFragment.java
+++ app/src/main/java/im/ldgd/com/myim/controller/fragment/ContactListFragment.java
@@ -1,26 +1,52 @@
 package im.ldgd.com.myim.controller.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
+
+import com.hyphenate.easeui.ui.EaseContactListFragment;
 
 import im.ldgd.com.myim.R;
+import im.ldgd.com.myim.controller.activity.AddContactActivity;
 
 /**
  * Created by ldgd on 2017/9/27.
  */
 
-public class ContactListFragment extends Fragment {
+public class ContactListFragment extends EaseContactListFragment {
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = View.inflate(getActivity(), R.layout.fragment_contact_list,null);
+    protected void initView() {
+        super.initView();
 
-        //    Toast.makeText(getActivity(),"ContactListFragment",Toast.LENGTH_SHORT).show();
-        return view;
+        // 标题栏显示加号
+        titleBar.setRightImageResource(R.drawable.em_add);
+
+        // 添加头布局
+        View headerView = View.inflate(getActivity(), R.layout.header_fragment_contact, null);
+        listView.addHeaderView(headerView);
+
+
     }
+
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+
+        titleBar.setRightLayoutClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
+
+
+
 }
